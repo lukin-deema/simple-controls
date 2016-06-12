f3d5b8ab71e5b64d671a89eb45c6ff9c0ed90385
Lukin <lukin.d8709@gmail.com>
2016-06-12 16:09:20 +0300

    add option track and hookOnCreate in tooltip

--- index.js
+++ index.js
@@ -26,3 +26,31 @@ var ttOption2 = {
 }
 var tt1 = new SimpleToolTip(ttOption1);
 var tt2 = new SimpleToolTip(ttOption2);
+
+///////////// TT
+var ttOption3 = {
+	hookElem: document.querySelector("#ttTextTest1"),
+	toolTipId: "id3", 
+	hookAttr: "value",
+	track: true, 
+	hookOnCreate: true,
+}
+var ttOption4 = {
+	hookElem: document.querySelector("#ttTextTest2"), // !! nodefault
+	toolTipId: "id4", // default "toolTip"
+	// !! toolTipText or hookAttr, toolTipText high priority (tooltip text)
+	// toolTipText: "Hide notify" // !! nodefault
+	hookAttr: "value",	// !! nodefault
+	track: true, /// default false (for tracking changes in textbox)
+	hookOnCreate: false, /// default true (on false need to call hook())
+}
+
+var tt1 = new SimpleToolTip(ttOption3);
+var tt2 = new SimpleToolTip(ttOption4);
+function addTT() {
+	tt2.add();
+}
+function destroyTT() {
+	tt2.destroy();
+}
+
--- simpleToolTip.js
+++ simpleToolTip.js
@@ -14,43 +14,77 @@
 			console.error("You should define 'options.hookElem'");
 		}		
 		this.options.hookElem = opt.hookElem;
-		this.options.toolTipId = opt.toolTipId || "toolTip";
 		if (!opt.toolTipText && !opt.hookAttr) {
 			console.error("You should define 'options.toolTipText' or 'options.hookAttr'");
 		}
 		this.options.toolTipText = opt.toolTipText;
 		this.options.hookAttr = opt.hookAttr;
+
+		this.options.toolTipId = opt.toolTipId || "toolTip";
+		this.options.track = opt.track != undefined ? opt.track : false;
+		this.options.hookOnCreate = opt.hookOnCreate != undefined ? opt.hookOnCreate : true;
+		if (options.hookOnCreate) {
+			this.hook();
+		}
 	}
 
+	function onMouseOver(e){
+		var elem = document.querySelector("#"+e.target.getAttribute('toolTipId'));
+		elem.style.display = "block";
+	}
+	function onMouseLeave(e){
+		var elem = document.querySelector("#"+e.target.getAttribute('toolTipId'));
+		elem.style.display = "none";
+	}
+	function onChange(e){
+		e.target.setAttribute('value', e.target.value);
+	}
+	function onMouseMove(e){
+			var elem = document.querySelector("#"+e.target.getAttribute('toolTipId'));
+			elem.innerHTML = e.target.getAttribute('value');
+			elem.style.top = (e.pageY - 20)+"px";
+			elem.style.left = (e.pageX + 5)+"px";
+		}
 	function SimpleToolTip(options) {
 		this.optionsSet(options);
-		this.toolTipContainer = document.createElement('div');
-		this.toolTipContainer.setAttribute('id', this.options.toolTipId);
-		this.toolTipContainer.innerHTML = options.toolTipText || options.hookElem[options.hookAttr];
-		applyStyles(this.toolTipContainer, {
-			"display": "none", 'background-color': '#ffffff', "position": "absolute"
-		})
+	}
+	function hook() {
+		if (!this.toolTipContainer) {
+			this.toolTipContainer = document.createElement('div');
+			this.toolTipContainer.setAttribute('id', this.options.toolTipId);
+			this.toolTipContainer.innerHTML = this.options.toolTipText || this.options.hookElem[this.options.hookAttr];
+			applyStyles(this.toolTipContainer, {
+				"display": "none", 'background-color': '#ffffff', "position": "absolute"
+			})
 
-		document.body.appendChild(this.toolTipContainer);
-
-		this.options.hookElem.setAttribute('toolTipId', this.options.toolTipId);
-		this.options.hookElem.addEventListener('mousemove', function(e){
-			var elem = document.querySelector("#"+e.target.getAttribute('toolTipId'));
-			elem.style.top = (e.pageY-20)+"px";
-			elem.style.left = (e.pageX)+"px";
-		});
-		this.options.hookElem.addEventListener('mouseover', function(e){
-			var elem = document.querySelector("#"+e.target.getAttribute('toolTipId'));
-			elem.style.display = "block";
-		});
-		this.options.hookElem.addEventListener('mouseleave', function(e){
-			var elem = document.querySelector("#"+e.target.getAttribute('toolTipId'));
-			elem.style.display = "none";
-		});
+			document.body.appendChild(this.toolTipContainer);
+			this.options.hookElem.setAttribute('toolTipId', this.options.toolTipId);
+			this.options.hookElem.setAttribute('value', this.toolTipContainer.innerHTML);
+			this.options.hookElem.addEventListener('mousemove', onMouseMove);
+			this.options.hookElem.addEventListener('mouseover', onMouseOver);
+			this.options.hookElem.addEventListener('mouseleave', onMouseLeave)
+			if (this.options.track) {
+				this.options.hookElem.addEventListener('change', onChange);
+			}
+		}
+	}
+	function destroy(){
+		this.options.hookElem.removeEventListener('mousemove', onMouseMove);
+		this.options.hookElem.removeEventListener("mouseover",onMouseOver);
+		this.options.hookElem.removeEventListener("mouseleave",onMouseLeave);
+		if (this.options.track) {
+			this.options.hookElem.removeEventListener("change",onChange);
+		}
+		this.options.hookElem.removeAttribute('toolTipId');
+		this.options.hookElem.removeAttribute('value');
+		this.toolTipContainer.parentNode.removeChild(this.toolTipContainer);
+		this.toolTipContainer = undefined;
 	}
 	SimpleToolTip.prototype = { 
 		optionsGet: optionsGet,
 		optionsSet: optionsSet,
+		hook: hook,
+		destroy: destroy,
 	};
 
 	global.SimpleToolTip = SimpleToolTip;
